147a5fb718c7d2b36f2fbc8124d222767f851d01
Prasanna <me210003054@iiti.ac.in>
2023-12-27 16:50:58 +0530

    FINAL SUBMISSION

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import NavBar from "./NavBar";
-
+//newsAPI asks for premium subscription if we try to fetch news on hosted website (eg on netlify)
 //for getting API calls after deploying on netlify, you can change the initial url of newsAPI with a premium ID in NavBar.jsx
 //Note that this has to be done at both the places
 function App() {
